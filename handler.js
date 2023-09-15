f20a408ced4d12d1a561125e36cb890bab76f990
Fehintola Akhumere <102443135+f3hint0la@users.noreply.github.com>
2023-09-15 13:29:03 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,12 +1,4 @@
-export default async function handler(request, response) {
-  if (!request.url) return response.status(400);
- 
-  const url = new URL(request.url, `http://${request.headers.host}`);
-  const { searchParams } = url;
-  const hasTitle = searchParams.has('title');
-  const title = hasTitle
-    ? searchParams.get('title')?.slice(0, 100)
-    : 'Welcome!!!';
- 
-  return response.status(200).json({ title });
+export default function handler(request, response) {
+  const { name = 'User' } = request.query;
+  return response.send(`Hello ${name}!`);
 }
